f32c1e62a7d7a156d43dee85d1b4237652cf85e9
stopvoldemort <david.squires1@gmail.com>
2017-09-08 11:11:39 -0400

    coding challenge

--- author.rb
+++ author.rb
@@ -1,1 +1,39 @@
+class Author
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select {|b| b.author == self.name}
+  end
+
+  def write_book(title, word_count)
+    Book.new(title, self.name, word_count)
+  end
+
+  def total_words
+    sum(self.books.map {|b| b.word_count})
+  end
+
+  def self.most_words
+    hashes = self.all.map do |a|
+      {a => a.total_words}
+    end
+    hashes.max_by{|k,v| v}.keys[0]
+  end
+
+  def sum(array)
+    array.inject(0){|sum,x| sum + x }
+  end
+
+end
